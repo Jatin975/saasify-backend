06623669395c2e15c8b110df786a3dd6d166a27b
Jatin975 <jatinpaul443@gmail.com>
2026-08-02 14:31:53 +0530

    feat: complete Day 2 setup with Prisma driver adapter and Postgres database connection

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,31 @@
-import express, {Application, Request, Response} from 'express';
+import express, { Application, Request, Response } from "express";
+import prisma from "./config/db";
 
 const app: Application = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.get('/health', (req: Request, res: Response)=>{
+app.get("/health", async (req: Request, res: Response) => {
+  try {
+    const result = await prisma.$queryRaw`SELECT 1`
+    console.log(`query res ${JSON.stringify(result)}`);
+
     res.status(200).json({
-        status: "success",
-        message: "SaasSify server is health and running",
-        timestamp: new Date().toISOString()
+      status: "success",
+      message: "SaasSify server is health and running",
+      timestamp: new Date().toISOString(),
     });
+
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Database connection failed",
+      error: error instanceof Error ? error.message : "Unknown error",
+    });
+  }
 });
 
-app.listen(PORT, ()=>{
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
